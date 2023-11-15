2d35d8d9ed80c4cac3e633acdf7f8824ea2208e0
Amir Famili <44036070+amirmohammadfamili@users.noreply.github.com>
2023-11-15 14:39:36 -0600

    Update script.js
    use ajax for load data

--- js/script.js
+++ js/script.js
@@ -10,13 +10,43 @@ $(document).ready(function() {
             });
         }
     });
-});
+
 
     $(window).on("load", function () {
         setTimeout(() => {
             $('#preloader').css('display', 'none');
         }, 800);
+         loadData();
     });
+
+ function loadData() {
+        $.ajax({
+            url: "../../Json/data.json",
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) { 
+                 var htmlStr="";
+                myJsonData = data.data;
+                myJsonData.map(project => {
+                    htmlStr+= `
+                           <div data-aos="fade-up"
+                                data-aos-anchor-placement="top-bottom" id="project"  className="">
+                                <img src=${project.img} alt=${project.name}/>
+                                <h4  className=" p-3">${project.name}</h4>
+                                <button><a href=${project.link} 
+                                target="_blank">Show Code
+                            </a></button>
+                            </div>`;
+                })
+                $('.projects').html(htmlStr);
+            },
+            error: function () {
+                console.log("There was an error with data...");
+
+            }
+        });
+    }
+});
 
 $(function() {
     $('.btn-group-fab').on('click', function() {
